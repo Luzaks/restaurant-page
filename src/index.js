2fb39e6388a14a3096bf7da03664f6a95169122b
Luzaks <kanemekanik@gmail.com>
2020-04-06 23:31:38 -0500

    Feat: Add menu displaying code in JavaScript

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { listHome, listAbout, listMenu, listContact, header } from './header.js'
 import { backgroundLayer, mainTag, tabContainer, backgroundContainer } from  './background-container.js';
 import {home} from './home.js';
 import {about} from "./about";
+import {menu} from './menu.js';
 
 
 const bodyTag = document.getElementsByTagName('body')[0];
@@ -26,6 +27,8 @@ listAbout.addEventListener('click', () => {
 
 listMenu.addEventListener( 'click', () => {
     bodyTag.appendChild(header());
+    divContent.appendChild(backgroundContainer());
+    tabsContainer.appendChild(menu());
 });
 
 listContact.addEventListener('click', () => {
