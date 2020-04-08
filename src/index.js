7df436cb060e30d6011b55b2e4f90e514ff25f75
Luzaks <kanemekanik@gmail.com>
2020-04-07 20:01:31 -0500

    Fix: Erase bug for duplicates in index.js source file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { listHome, listAbout, listMenu, listContact, header } from './header.js';
 import { backgroundLayer, mainTag, tabContainer, backgroundContainer } from  './background-container.js';
 import {homeDiv, home} from './home.js';
-import {aboutDiv, about} from "./about";
+import {about} from "./about";
 import {menu} from './menu.js';
 import {contact} from './contact.js';
 
@@ -13,6 +13,9 @@ bodyTag.appendChild(header());
 divContent.appendChild(backgroundContainer());
 const tabsContainer = document.querySelector('.tabs');
 tabsContainer.appendChild(home());
+tabsContainer.appendChild(about());
+tabsContainer.appendChild(menu());
+tabsContainer.appendChild(contact());
 
 listHome.addEventListener('click', () => {
     const targetHome = document.querySelector(listHome.dataset.tabTarget);
@@ -21,39 +24,32 @@ listHome.addEventListener('click', () => {
         tabContents.classList.remove('active');
     });
     targetHome.classList.add('active');
-    console.log(tabContents);
 });
 
 listAbout.addEventListener('click', () => {
-    tabsContainer.appendChild(about());
     const targetAbout = document.querySelector(listAbout.dataset.tabTarget);
     const tabContents = document.querySelectorAll('[data-tab-content]');
     tabContents.forEach(tabContents => {
         tabContents.classList.remove('active');
     });
     targetAbout.classList.add('active');
-    console.log(tabContents);
 });
 
 listMenu.addEventListener( 'click', () => {
-    tabsContainer.appendChild(menu());
     const targetMenu = document.querySelector(listMenu.dataset.tabTarget);
     const tabContents = document.querySelectorAll('[data-tab-content]');
     tabContents.forEach(tabContents => {
         tabContents.classList.remove('active');
     });
     targetMenu.classList.add('active');
-    console.log(tabContents);
 });
 
 listContact.addEventListener('click', () => {
-    tabsContainer.appendChild(contact());
     const targetContact = document.querySelector(listContact.dataset.tabTarget);
     const tabContents = document.querySelectorAll('[data-tab-content]');
     tabContents.forEach(tabContents => {
         tabContents.classList.remove('active');
     });
     targetContact.classList.add('active');
-    console.log(tabContents);
 
 });
